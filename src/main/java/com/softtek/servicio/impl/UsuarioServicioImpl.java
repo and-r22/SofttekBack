b9b246dca6af3cf599e5c3789077a3182c2e49c0
Mustafa <78199096+Mustafa-Elout@users.noreply.github.com>
2022-06-09 12:44:17 +0200

    Add files via upload

--- src/main/java/com/softtek/servicio/impl/UsuarioServicioImpl.java
+++ src/main/java/com/softtek/servicio/impl/UsuarioServicioImpl.java
@@ -13,11 +13,13 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.softtek.modelo.Usuario;
+import com.softtek.repo.IGenericRepo;
 import com.softtek.repo.IUsuario;
+import com.softtek.servicio.IUsuarioServicio;
 
 @Service
-public class UsuarioServicioImpl implements UserDetailsService {
-	
+public class UsuarioServicioImpl extends CRUDImp<Usuario, Integer> implements UserDetailsService, IUsuarioServicio {
+
 	@Autowired
 	private IUsuario repo;
 
@@ -25,21 +27,45 @@ public class UsuarioServicioImpl implements UserDetailsService {
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
 		Usuario usuario = repo.findOneByUsername(username);
-		
-		if(usuario == null) {
+
+		if (usuario == null) {
 			throw new UsernameNotFoundException(String.format("Usuario no existe", username));
 		}
-		
+
 		List<GrantedAuthority> roles = new ArrayList<>();
-		
+
 		usuario.getRoles().forEach(rol -> {
 			roles.add(new SimpleGrantedAuthority(rol.getNombre()));
 		});
-		
-		UserDetails ud = new User(usuario.getUsername(), usuario.getPassword(), usuario.isEnabled(), true, true, true, roles);
-		
+
+		UserDetails ud = new User(usuario.getUsername(), usuario.getPassword(), usuario.isEnabled(), true, true, true,
+				roles);
+
 		return ud;
-		
+
 	}
+
+	@Override
+	protected IGenericRepo<Usuario, Integer> getRepo() {
+		// TODO Auto-generated method stub
+		return repo;
+	}
+
+//	@Override
+//	public String encriptar(Usuario usuario) {
+//		byte[] salt = new String(usuario.getPassword()).getBytes();
+//		int iterationCount = 40000;
+//		int keyLength = 128;
+//		String password = usuario.getPassword();
+//		SecretKeySpec key = createSecretKey(password.toCharArray(), salt, iterationCount, keyLength);
+//
+//		String originalPassword = password;
+//		System.out.println("Original password: " + originalPassword);
+//		String encryptedPassword = encrypt(originalPassword, key);
+//		System.out.println("Encrypted password: " + encryptedPassword);
+//		String decryptedPassword = decrypt(encryptedPassword, key);
+//		System.out.println("Decrypted password: " + decryptedPassword);
+//		return null;
+//	}
 
 }
